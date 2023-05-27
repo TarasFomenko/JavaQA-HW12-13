d885f60e1e46c07e2223c3fa066bcbca96cadc1c
Taras Fomenko <fomenkotarasevg@gmail.com>
2023-05-27 02:44:17 +0200

    HW 13 - Finished

--- src/main/java/Homework13/Homework13.java
+++ src/main/java/Homework13/Homework13.java
@@ -1,24 +1,19 @@
 package Homework13;
 
-import Configuration.WEBDRIVERS;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 import static Configuration.BaseClass.driver;
 
 public class Homework13 {
     public static final String urlHillel = "https://ithillel.ua/";
-    public static final String hillelTitle ="Комп'ютерна школа Hillel Online: Курси IT-технологій";
+    public static final String hillelTitle = "Комп'ютерна школа Hillel Online: Курси IT-технологій";
 
     public static void main(String[] args) throws InterruptedException {
         driver.get(urlHillel);
-        if(!driver.getTitle().equals(hillelTitle)){
+        if (!driver.getTitle().equals(hillelTitle)) {
             System.out.println("Wrong page");
             driver.quit();
         }
@@ -30,8 +25,23 @@ public class Homework13 {
         Thread.sleep(1000);
         WebElement frontEndBasic = driver.findElement(By.xpath("//*[@id=\"block-202987\"]//a[@href='https://ithillel.ua/courses/front-end-basic']"));
         frontEndBasic.click();
-        Thread.sleep(5000);
+        Thread.sleep(2000);
+        //Добрались до начала списка учителей
+        ScrollToElement.scrollToElementVisibilityOf(driver, new By.ByXPath("//ul[@class ='coach-list coaches_list']"));
 
-        ScrollToElement.scrollToElementVisibilityOf(driver,new By.ByXPath("//*[@id=\"coachesSection\"]/div/div/div/h2"));
-    }
-}
+        //Нашли все элементы, которые нам нужны (Учителя)
+        List<WebElement> listOfAllTeachers = driver.findElements(By.xpath("//ul[@class ='coach-list coaches_list']/li/a/div/div/div/img"));
+        List<String> teachersNames = new ArrayList<>();
+
+        //Вытягиваем нужный нам атрибут из списка и ложим в List
+        for (WebElement element : listOfAllTeachers) {
+            String attributeValue = element.getAttribute("alt");
+            teachersNames.add(attributeValue);
+        }
+
+        //Выводим список в консоль
+        for (String value : teachersNames) {
+            System.out.println(value);
+        }
+
+    }}
--- src/main/java/Homework13/ScrollToElement.java
+++ src/main/java/Homework13/ScrollToElement.java
@@ -2,13 +2,8 @@ package Homework13;
 
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
 
 
 public class ScrollToElement {
@@ -23,8 +18,12 @@ public class ScrollToElement {
                 bool = true;
             } catch (Exception ignore) {
                 js.executeScript("window.scrollBy(0,1000)");
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
 }
-
